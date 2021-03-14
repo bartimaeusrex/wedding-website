6c76b4befc87160204a857bcb04a98fc5b746732
bartimaeusrex <mrpatrickreinhart@gmail.com>
2021-03-13 21:24:16 -0500

    Trying to implement JotForm code so I can strip the ad at the bottom

---
+++ form.js
@@ -0,0 +1,17 @@
+JotForm.init(function(){
+  setTimeout(function() {
+      $('input_29').hint('ex: myname@example.com');
+   }, 20);
+  setTimeout(function() {
+      $('input_87').hint('ex: 2');
+   }, 20);
+if (window.JotForm && JotForm.accessible) $('input_15').setAttribute('tabindex',0);
+JotForm.newDefaultTheme = false;
+JotForm.extendsNewTheme = false;
+JotForm.newPaymentUIForNewCreatedForms = false;
+/*INIT-END*/
+});
+
+JotForm.prepareCalculationsOnTheFly([null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"comments","qid":"15","subLabel":"Please Enter any questions or concerns that you dont see listed on the form","text":"Comments","type":"control_textarea"},null,null,null,null,null,null,null,null,null,null,null,null,{"name":"fullName28","qid":"28","text":"Full Name","type":"control_fullname"},{"name":"email29","qid":"29","text":"E-mail","type":"control_email"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"clickTo53","qid":"53","text":"R.S.V.P","type":"control_head"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"howMany87","qid":"87","subLabel":"","text":"How many in your party","type":"control_number"},null,null,null,null,null,{"description":"","name":"willYou93","qid":"93","text":"Will you be able to join us?","type":"control_radio"},null,{"name":"submit","qid":"95","text":"Submit","type":"control_button"}]);
+setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"comments","qid":"15","subLabel":"Please Enter any questions or concerns that you dont see listed on the form","text":"Comments","type":"control_textarea"},null,null,null,null,null,null,null,null,null,null,null,null,{"name":"fullName28","qid":"28","text":"Full Name","type":"control_fullname"},{"name":"email29","qid":"29","text":"E-mail","type":"control_email"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"clickTo53","qid":"53","text":"R.S.V.P","type":"control_head"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"name":"howMany87","qid":"87","subLabel":"","text":"How many in your party","type":"control_number"},null,null,null,null,null,{"description":"","name":"willYou93","qid":"93","text":"Will you be able to join us?","type":"control_radio"},null,{"name":"submit","qid":"95","text":"Submit","type":"control_button"}]);}, 20); 
